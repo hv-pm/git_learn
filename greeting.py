48bb5a76d34abe7f0dd41ce4b36bc11c93061126
Hygor Vinicius <hv-pm@hotmail.com>
2023-02-07 11:07:26 -0300

    Print text updated

--- greeting.py
+++ greeting.py
@@ -2,11 +2,11 @@ currentTime = input("Insira a hora atual: ")
 
 if currentTime.isdigit():
     if int(currentTime) >= 0 and int(currentTime) <= 11 or int(currentTime) == 24:
-        print("Bom dia.")
+        print("Bom dia!")
     elif int(currentTime) >= 12 and int(currentTime) <= 17:
-        print("Boa tarde.")
+        print("Boa tarde!")
     elif int(currentTime) >= 18 and int(currentTime) <= 23:
-        print("Boa noite.")
+        print("Boa noite!")
 else:
     print("Por favor, insira um número.")
 
