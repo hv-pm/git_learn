45b31b670eff6d5cbff44a87ccb85dc07d7c8eab
Hygor Vinicius <hv-pm@hotmail.com>
2023-02-07 11:28:30 -0300

    New condition for midnight added

--- greeting.py
+++ greeting.py
@@ -1,12 +1,14 @@
 currentTime = input("Insira a hora atual: ")
 
 if currentTime.isdigit():
-    if int(currentTime) >= 0 and int(currentTime) <= 11 or int(currentTime) == 24:
+    if int(currentTime) >= 0 and int(currentTime) <= 11:
         print("Bom dia!")
     elif int(currentTime) >= 12 and int(currentTime) <= 17:
         print("Boa tarde!")
     elif int(currentTime) >= 18 and int(currentTime) <= 23:
         print("Boa noite!")
+    elif int(currentTime) == 24:
+        print("É madrugada!")
 else:
     print("Por favor, insira um número.")
 
